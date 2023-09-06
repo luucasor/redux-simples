a80579baaf852dbfde897dafec16ace24e242295
luucasor <lucas@jornadatecnologia.com>
2023-09-06 16:03:54 -0300

    Definindo gerenciamento de estado local, com comunicação direta e indireta, que será posteriormente migrado para redux

---
+++ src/components/Media.jsx
@@ -0,0 +1,17 @@
+import Card from './Card'
+
+import React from 'react'
+
+export default props => {
+    const { min, max } = props
+    return (
+        <Card title="Média dos Números" green>
+            <div>
+                <span>
+                    <span>Resultado:</span>
+                    <strong>{(max + min)/2}</strong>
+                </span>
+            </div>
+        </Card>
+    )
+}
---
+++ src/components/Soma.jsx
@@ -0,0 +1,17 @@
+import Card from './Card'
+
+import React from 'react'
+
+export default props => {
+    const { min, max } = props
+    return (
+        <Card title="Soma dos Números" blue>
+            <div>
+                <span>
+                    <span>Resultado:</span>
+                    <strong>{ max + min }</strong>
+                </span>
+            </div>
+        </Card>
+    )
+}
